df1177a915df6e7dc9e15de6f26568d9b98806e7
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-05-14 17:57:29 +0530

    Refactored build files to use version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
-    id("com.google.dagger.hilt.android") version "2.55" apply false
+    alias(libs.plugins.devtools.ksp) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
