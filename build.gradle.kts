61b3a25bd621325b030eec33c53c46843950c351
Md Adnan <125251426+adnanrangrej@users.noreply.github.com>
2025-05-14 17:42:07 +0530

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
     id("com.google.dagger.hilt.android") version "2.55" apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
